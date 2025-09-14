b017f0ef48841361899f5de1a98cd72d0e142859
DaBrawler <d.s.demango@gmail.com>
2025-09-14 13:12:47 -0500

    Fix import errors and install missing dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Page from "./pages/ai-portal";
+import { AIPortal as Page } from "./pages/ai-portal";
 import "./index.css"; // ok if this file doesn't exist
 ReactDOM.createRoot(document.getElementById("root")!).render(<Page />);
